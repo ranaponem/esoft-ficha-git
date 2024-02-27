bc53089ba82e0427ee13bc6c962ce3a528f95c3d
Rafael Saraiva <rpsaraiva@proton.me>
2024-02-27 12:58:24 +0000

    adicionar o meu nome

--- src/Frame.java
+++ src/Frame.java
@@ -4,7 +4,10 @@ public class Frame extends JFrame{
     Frame(){
         JPanel p1 = new JPanel();
         JLabel l1 = new JLabel("Hello World!");
+        JLabel l2 = new JLabel("O Rafael Saraiva esteve aqui");
         l1.setVerticalAlignment(SwingConstants.CENTER);
+        l2.setVerticalAlignment(SwingConstants.CENTER);
+        l2.setHorizontalAlignment(SwingConstants.CENTER);
         p1.add(l1);
         add(p1);
         pack();
